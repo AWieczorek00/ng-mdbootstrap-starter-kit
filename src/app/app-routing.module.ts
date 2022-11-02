f9cf853ed721ee3850917788bf9f71ac29ec486e
AWieczorek00 <awieczorek0000@gmail.com>
2022-11-02 15:11:01 +0100

    02.11.2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './pages/home/home.page';
+import { HomePageModule } from './pages/home/home.page-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', loadChildren:()=> HomePageModule }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), HomePageModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
